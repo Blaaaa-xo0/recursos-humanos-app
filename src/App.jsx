1df0deb557c2dc14a1352d984b050300a5570ba0
brayanfe2009 <brayanfe2009@gmail.com>
2024-05-28 06:33:40 +0000

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './template/Navbar'
+import ListaEmpleados from './empleados/ListaEmpleados'
+import AgregarEmpleados from './empleados/AgregarEmpleados'
+
 
 function App() {
   
 
   return (
     <>
+      
       <Navbar />
       <Routes>
-        <Route path='/' element={<h1>home</h1>} />
+        <Route path='/' element={<ListaEmpleados/>} />
+        <Route path='/' element={<AgregarEmpleados/>} />
       </Routes>
       
     </>
--- src/empleados/AgregarEmpleados.jsx
+++ src/empleados/AgregarEmpleados.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function AgregarEmpleados() {
     return (
         <div>
-            
+            agregar empleados
         </div>
     )
 }
